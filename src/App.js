b75c4cbcd1e7925fb2b6b0486c1d8937b30bdb14
polikk <polikigor@ukr.net>
2021-04-21 13:41:56 +0300

    Create sign up page + add register to firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 import * as ROUTER from './constants/routes'
 
 const Login = lazy(()=> import('./pages/login'));
+const SignUp = lazy(()=> import('./pages/sign-up'));
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Suspense fallback={<div>Загрузка...</div>}>
         <Switch>
           <Route path={ROUTER.LOGIN} component={Login}/>
+          <Route path={ROUTER.SIGN_UP} component={SignUp}/>
         </Switch>
       </Suspense>
     </Router>
