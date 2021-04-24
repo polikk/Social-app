5447dddc1a49546701168c261fcfd1c402f894a1
polikk <polikigor@ukr.net>
2021-04-24 18:56:28 +0300

    Add sidebar component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React,{lazy,Suspense} from 'react';
 import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 import * as ROUTER from './constants/routes'
+import UserContext from './context/user';
+import useAuthListener from './hooks/use-auth-listener';
 
 const Login = lazy(()=> import('./pages/login'));
 const SignUp = lazy(()=> import('./pages/sign-up'));
+const NotFound = lazy(()=> import('./pages/not-found'));
+const Dashboard = lazy(()=> import('./pages/dashboard'));
 
 function App() {
+  const {user} = useAuthListener();
   return (
-    <Router>
-      <Suspense fallback={<div>Загрузка...</div>}>
-        <Switch>
-          <Route path={ROUTER.LOGIN} component={Login}/>
-          <Route path={ROUTER.SIGN_UP} component={SignUp}/>
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={user}>
+        <Router>
+          <Suspense fallback={<div>Loading...</div>}>
+           <Switch>
+            <Route path={ROUTER.LOGIN} component={Login} exact/>
+            <Route path={ROUTER.SIGN_UP} component={SignUp} exact/>
+            <Route path={ROUTER.DASHBOARD} component={Dashboard} exact/>
+            <Route  component={NotFound}/>
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
-
 export default App;
---
+++ src/pages/not-found.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import Header from '../components/header';
+
+export default function NotFound() {
+  useEffect(() => {
+    document.title = 'Not Found - Instagram';
+  }, []);
+
+  return (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="mx-auto max-w-screen-lg">
+        <p className="text-center text-2xl">Not Found!</p>
+      </div>
+    </div>
+  );
+}
