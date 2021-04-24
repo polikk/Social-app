43b58616abbed2a2ff30e73ae6815037a617c3ef
polikk <polikigor@ukr.net>
2021-04-24 22:34:19 +0300

    create usePhoto hook

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import { useContext } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import LoggedInUserContext from '../context/logged-in-user';
+import usePhotos from '../hooks/use-photos';
+import Post from './post';
 
+export default function Timeline() {
+  const { user } = useContext(LoggedInUserContext);
+  const { photos } = usePhotos(user);
 
-export default function Timeline(){
-    return(
-        <p>Hello</p>
-    );
+  return (
+    <div className="container col-span-2">
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : (
+        photos.map((content) => <Post key={content.docId} content={content} />)
+      )}
+    </div>
+  );
 }
